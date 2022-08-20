a979e207ab8349190c407503d0ddb0e677383a8f
profoundsea25 <profoundsea25@gmail.com>
2022-08-20 19:26:08 +0900

    [Real-World Software Development] Init

---
+++ Real-World Software Development/src/main/java/book/realworldsoftwaredevelopment/RealWorldSoftwareDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package book.realworldsoftwaredevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldSoftwareDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealWorldSoftwareDevelopmentApplication.class, args);
+    }
+
+}
---
+++ Real-World Software Development/src/test/java/book/realworldsoftwaredevelopment/RealWorldSoftwareDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package book.realworldsoftwaredevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldSoftwareDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

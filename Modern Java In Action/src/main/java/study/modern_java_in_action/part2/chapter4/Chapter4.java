9613f0013baecf9bc28497e3c3ecd3b250d06b74
profoundsea25 <profoundsea25@gmail.com>
2022-10-21 00:23:39 +0900

    [Modern Java In Action] Chapter 4. 스트림 소개 (p.141~149)

--- Modern Java In Action/src/main/java/study/modern_java_in_action/part2/chapter4/Chapter4.java
+++ Modern Java In Action/src/main/java/study/modern_java_in_action/part2/chapter4/Chapter4.java
@@ -3,11 +3,9 @@ package study.modern_java_in_action.part2.chapter4;
 import org.junit.jupiter.api.Test;
 import study.modern_java_in_action.model.Dish;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Chapter 4. 스트림 소개
@@ -84,7 +82,128 @@ public class Chapter4 {
      */
     /* 스트림이란 정확히 무엇인가?
         - 스트림이란 '데이터 처리 연산을 지원하도록 소스에서 추출된 연속된 요소(Sequence of elements)'로 정의할 수 있다.
-            -
+            - 연속된 요소
+                - 스트림은 특정 요소 형식으로 이루어진 연속된 값 집합의 인터페이스를 제공한다. (컬렉션과 마찬가지다.)
+                - filter, sorted, map 처럼 표현 계산식이 주를 이룬다.
+            - 소스
+                - 스트림은 컬렉션, 배열, I/O 자원 등의 데이터 제공 소스로부터 데이터를 소비한다.
+                - 정렬된 컬렉션으로 스트림을 생성하면 정렬이 그대로 유지된다.
+            - 데이터 처리 연산
+                - 함수형 프로그래밍 언어에서 일반적으로 지원하는 연산과 데이터베이스와 비슷한 연산을 지원한다.
+                - 순차적 또는 병렬로 실행할 수 있다.
+        - 스트림의 중요한 2가지 특징
+            - 파이프라이닝
+                - 스트림 연산끼리 연결해서 커다란 파이프 라인을 구성할 수 있도록 스트림 자신을 반환한다.
+                - SQL 과 비슷하다.
+            - 내부 반복
+     */
+    @Test
+    void streamExample() {
+        List<Dish> menu = Dish.exampleDishList();
+        List<String> threeHighCaloricDishNames = menu.stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .map(Dish::getName)
+                .limit(3)
+                .collect(Collectors.toList());
+        System.out.println(threeHighCaloricDishNames);
+    }
 
+    /* 스트림 대표 메서드
+        - filter : 람다를 인수로 받아 스트림에서 특정 요소를 제외시킨다.
+        - map : 람다를 이용해서 한 요소를 다른 요소로 변환하거나 정보를 추출한다.
+        - limit : 정해진 개수 이상의 요소가 스트림에 저장되지 못하게 스트림 크기를 축소한다.
+        - collect : 스트림을 다른 형식으로 변환한다.
+     */
+
+
+    /**
+     * 4.3 스트림과 컬렉션
+     */
+
+    /* 스트림과 컬렉션
+        - 컬렉션과 스트림 모두 연속된 요소 형식의 값을 저장하는 자료구조의 인터페이스를 제공한다.
+            - '연속된'이란 순서와 상관없이 아무 값에서 접속하는 것이 아니라 순차적으로 값에 접근한다는 것을 의미한다.
+        - 데이터를 언제 계산하느냐가 컬렉션과 스트림의 가장 큰 차이다.
+            - 컬렉션은 현재 자료구조가 포함하는 모든 값을 메모리에 저장하는 자료구조다.
+                - 즉, 모든 요소는 컬렉션에 추가하기 전에 계산되어야 한다.
+                - 컬렉션에 요소를 추가하거나 삭제할 수 있다. 이런 연산을 수행할 때마다 컬렉션의 모든 요소를 메모리에 저장해야 한다.
+                - 컬렉션에 추가하려는 요소는 미리 계산되어야 한다.
+                - 적극적으로 생성된다. (팔기도 전에 창고를 가득 채움)
+            - 스트림은 이론적으로 요청할 때만 요소를 계산하는 고정된 자료구조다.
+                - 스트림에서 요소를 추가하거나 스트림에서 요소를 제거할 수 없다.
+                - 사용자가 요청하는 값만 스트림에서 추출한다.
+                - 게으르게 만들어진다.
+     */
+
+    /* 4.3.1 딱 한 번만 탐색할 수 있다
+        - 반복자와 마찬가지로 스트림도 한 번만 탐색할 수 있다.
+            - 즉, 탐색된 스트림의 요소는 소비된다.
+            - 한 번 탐색한 요소를 다시 탐색하려면 초기 데이터 소스에서 새로운 스트림을 만들어야 한다.
+     */
+    @Test
+    void streamConsumeTest() {
+        List<String> title = Arrays.asList("Java8", "In", "Action");
+        Stream<String> s = title.stream();
+        s.forEach(System.out::println); // 출력
+        s.forEach(System.out::println); // 스트림이 이미 소비되었으므로(닫혔으므로) Exception 발생
+    }
+
+    /* 4.3.2 외부 반복과 내부 반복
+        - 컬렉션에선 사용자가 직접 요소를 반복해야 한다. 이를 외부 반복이라 한다.
+            - 명시적으로 컬렉션 항목을 하나씩 가져와서 처리한다.
+            - 병렬성을 스스로 관리해야 한다.
+        - 스트림에서는 반복을 알아서 처리하고 결과 스트림값을 어딘가에 저장해주는 내부 반복을 사용한다.
+            - 함수에 어떤 작업을 수행할지만 지정하면 모든 것이 알아서 처리된다.
+            - 외부 반복보다 더 쉽게 작업을 투명하게 병렬로 처리하거나 더 최적화된 다양한 순서로 처리할 수 있다.
+            - 데이터 표현과 하드웨어를 활용한 병렬성 구현을 자동으로 선택한다.
+     */
+    @Test
+    void iterationExample() {
+        List<Dish> menu = Dish.exampleDishList();
+
+        // 외부 반복 - for-each 구문
+        List<String> names1 = new ArrayList<>();
+        for (Dish dish : menu) {
+            names1.add(dish.getName());
+        }
+
+        // 외부 반복 - Iterator 객체 이용 (명시적 반복)
+        List<String> names2 = new ArrayList<>();
+        Iterator<Dish> iterator = menu.iterator();
+        while (iterator.hasNext()) {
+            Dish dish = iterator.next();
+            names2.add(dish.getName());
+        }
+
+        // 내부 반복 - Stream
+        List<String> names3 = menu.stream()
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    void quiz_4_1() {
+        List<Dish> menu = Dish.exampleDishList();
+
+        // 문제
+        List<String> highCaloricDishes = new ArrayList<>();
+        Iterator<Dish> iterator = menu.iterator();
+        while (iterator.hasNext()) {
+            Dish dish = iterator.next();
+            if (dish.getCalories() > 300) {
+                highCaloricDishes.add(dish.getName());
+            }
+        }
+
+        // 답
+        List<String> highCaloricDishesByStream = menu.stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+    }
+
+
+    /**
+     * 4.4 스트림 연산
      */
 }

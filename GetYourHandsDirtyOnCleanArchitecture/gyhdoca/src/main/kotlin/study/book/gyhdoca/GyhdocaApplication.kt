f5d94a4e74059e39978e8bba3bcdccba2290d5f8
profoundsea25 <profoundsea25@gmail.com>
2023-08-15 17:29:30 +0900

    [만들면서 배우는 클린 아키텍처] Init Commit

---
+++ GetYourHandsDirtyOnCleanArchitecture/gyhdoca/src/main/kotlin/study/book/gyhdoca/GyhdocaApplication.kt
@@ -0,0 +1,11 @@
+package study.book.gyhdoca
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GyhdocaApplication
+
+fun main(args: Array<String>) {
+    runApplication<GyhdocaApplication>(*args)
+}
---
+++ GetYourHandsDirtyOnCleanArchitecture/gyhdoca/src/test/kotlin/study/book/gyhdoca/GyhdocaApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.book.gyhdoca
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GyhdocaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
